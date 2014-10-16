9cd7740436c210fb70ae7d94a98cfea7063ad602
echo <echo@namsehyeon-ui-MacBook-Pro.local>
2014-10-16 15:48:38 +0900

    init project

---
+++ Soraiao/Soraiao/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Soraiao
+//
+//  Created by echo on 2014. 10. 16..
+//  Copyright (c) 2014년 echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Soraiao/Soraiao/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Soraiao
+//
+//  Created by echo on 2014. 10. 16..
+//  Copyright (c) 2014년 echo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
